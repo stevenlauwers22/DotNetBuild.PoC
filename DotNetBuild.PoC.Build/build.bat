304c5e21302508286dcae6f1209b207e9f9b968f
stevenlauwers22 <stevenlauwers22@hotmail.com>
2014-09-19 14:38:04 +0200

    Update to DotNetBuild 1.1.0

--- DotNetBuild.PoC.Build/build.bat
+++ DotNetBuild.PoC.Build/build.bat
@@ -1,2 +1,2 @@
-..\packages\DotNetBuild.Runner.Assembly.1.0.0\tools\net45\DotNetBuild.Runner.Assembly.exe assembly:.\bin\Debug\DotNetBuild.PoC.Build.dll target:ci configuration:defaultConfig
+..\packages\DotNetBuild.Runner.Assembly.1.1.0\tools\net45\DotNetBuild.Runner.Assembly.exe assembly:.\bin\Debug\DotNetBuild.PoC.Build.dll target:ci configuration:defaultConfig
 pause
